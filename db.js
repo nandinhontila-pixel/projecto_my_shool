22fbdee31a0113a4fd6b86a867750cc4dc4dd53b
nandinhontila-pixel <nandinhontila@gmail.com>
2026-07-03 00:36:13 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,19 @@
-const FIREBASE_CONFIG = {
-  apiKey:            "AIzaSy...",          // ← a tua chave
-  authDomain:        "ntila-estudos.firebaseapp.com",
-  projectId:         "ntila-estudos",
-  storageBucket:     "ntila-estudos.appspot.com",
-  messagingSenderId: "123456789",
-  appId:             "1:123456789:web:abc123"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBLJqiquB0Tfqm9ietzGF0nh07ATYkdSkM",
+    authDomain: "projecto-my-shool-38e65.firebaseapp.com",
+    projectId: "projecto-my-shool-38e65",
+    storageBucket: "projecto-my-shool-38e65.firebasestorage.app",
+    messagingSenderId: "998695548680",
+    appId: "1:998695548680:web:354d215a1536c90688debd"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
