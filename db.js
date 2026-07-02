50bf8aa0df9633adc66bc12a5af2773672cf1c32
nandinhontila-pixel <nandinhontila@gmail.com>
2026-07-02 22:48:37 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,8 @@
-// Configuração do Firebase atualizada para o teu portal de estudos
+// 1. Importar as funções necessárias dos módulos do Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+// 2. As tuas credenciais de configuração (as chaves do teu projeto)
 const firebaseConfig = {
   apiKey: "AIzaSyBb_R6NPgacf_bMT5kQEmI1Ji3mdbzDfZw",
   authDomain: "projecto-my-shool.firebaseapp.com",
@@ -9,8 +13,9 @@ const firebaseConfig = {
   measurementId: "G-FE0D9MMYC1"
 };
 
-// Inicializar o Firebase no formato compatível com o teu site
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
-const db = firebase.firestore();
+// 3. Inicializar o Firebase e o Banco de Dados (Firestore)
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// 4. Exportar a variável 'db' para usares noutros ficheiros do site
+export { db };
